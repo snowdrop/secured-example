bfe5dfc9c8da7eac7de07f203b7711d330ad172e
Charles Moulliard <cmoulliard@apache.org>
2016-12-14 18:38:17 +0100

    Uncomment line to create a user

--- scripts/add_user.sh
+++ scripts/add_user.sh
@@ -20,7 +20,7 @@ auth_result=$(http --verify=no -f $SSO_HOST/auth/realms/$REALM/protocol/openid-c
 access_token=$(echo -e "$auth_result" | awk -F"," '{print $1}' | awk -F":" '{print $2}' | sed s/\"//g | tr -d ' ')
 
 #echo ">>> Add user"
-# http --verify=no --verbose POST $SSO_HOST/auth/admin/realms/$REALM/users "Authorization: Bearer $access_token" < $SCRIPT_DIR/user.json
+http --verify=no --verbose POST $SSO_HOST/auth/admin/realms/$REALM/users "Authorization: Bearer $access_token" < $SCRIPT_DIR/user.json
 
 echo ">>> Get User id"
 userId=$(http --verify=no --verbose GET $SSO_HOST/auth/admin/realms/$REALM/users username==bburke "Authorization: Bearer $access_token" | jsonValue id)
