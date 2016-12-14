6d5451cdc320108450bc0d12246611fe12567481
Charles Moulliard <cmoulliard@apache.org>
2016-12-14 18:23:36 +0100

    Add missing scripts

---
+++ scripts/common.sh
@@ -0,0 +1,7 @@
+REALM=master
+USER=admin
+PASSWORD=admin
+CLIENT_ID=demoapp
+SECRET=cb7a8528-ad53-4b2e-afb8-72e9795c27c8
+SSO_HOST=${1:-https://secure-sso-sso.e8ca.engint.openshiftapps.com}
+APP=${2:-http://secured-springboot-rest-sso.e8ca.engint.openshiftapps.com}
