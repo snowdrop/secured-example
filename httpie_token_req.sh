dc5efd8e6d8a24625665816ede6a905bc0e26c2c
Ales Justin <ales.justin@gmail.com>
2016-12-14 13:51:39 +0100

    Add secured- prefix.

--- httpie_token_req.sh
+++ httpie_token_req.sh
@@ -6,7 +6,7 @@ PASSWORD=admin
 CLIENT_ID=demoapp
 SECRET=cb7a8528-ad53-4b2e-afb8-72e9795c27c8
 SSO_HOST=${1:-https://secure-sso-sso.e8ca.engint.openshiftapps.com}
-APP=${2:-http://springboot-rest-sso.e8ca.engint.openshiftapps.com}
+APP=${2:-http://secured-springboot-rest-sso.e8ca.engint.openshiftapps.com}
 
 echo ">>> HTTP Token query"
 echo "http --verify=no -f $SSO_HOST/auth/realms/$REALM/protocol/openid-connect/token username=$USER password=$PASSWORD client_secret=$SECRET grant_type=password client_id=$CLIENT_ID"
