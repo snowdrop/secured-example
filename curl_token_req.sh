8909463bb86ae99066f13662722dab7c3b776199
Charles Moulliard <cmoulliard@apache.org>
2016-12-14 10:55:27 +0100

    Improve bash scripts

--- curl_token_req.sh
+++ curl_token_req.sh
@@ -4,17 +4,20 @@ REALM=master
 USER=admin
 PASSWORD=admin
 CLIENT_ID=demoapp
-HOST=https://secure-sso-sso.e8ca.engint.openshiftapps.com
-CLIENT=http://springboot-rest-sso.e8ca.engint.openshiftapps.com
+SSO_HOST=${1:-https://secure-sso-sso.e8ca.engint.openshiftapps.com}
+APP=${2:-http://springboot-rest-sso.e8ca.engint.openshiftapps.com}
 
 echo ">>> HTTP Token query"
-echo "curl -X POST $HOST/auth/realms/$REALM/protocol/openid-connect/token -d grant_type=password -d username=$USER -d password=$PASSWORD -d client_id=$CLIENT_ID"
+echo "curl -X POST $SSO_HOST/auth/realms/$REALM/protocol/openid-connect/token -d grant_type=password -d username=$USER -d password=$PASSWORD -d client_id=$CLIENT_ID"
 
-auth_result=$(curl -sk -X POST $HOST/auth/realms/$REALM/protocol/openid-connect/token -d grant_type=password -d username=$USER -d password=$PASSWORD -d client_id=$CLIENT_ID)
+auth_result=$(curl -sk -X POST $SSO_HOST/auth/realms/$REALM/protocol/openid-connect/token -d grant_type=password -d username=$USER -d password=$PASSWORD -d client_id=$CLIENT_ID)
 access_token=$(echo -e "$auth_result" | awk -F"," '{print $1}' | awk -F":" '{print $2}' | sed s/\"//g | tr -d ' ')
 
 echo ">>> TOKEN Received"
 echo $access_token
 
-echo ">>> Greeting Service"
-curl -k $CLIENT/greeting -H "Authorization:Bearer $access_token"
+echo ">>> Greeting"
+curl -k $APP/greeting -H "Authorization:Bearer $access_token"
+
+echo ">>> Greeting Customized Message"
+curl -k $APP/greeting?name=Spring -H "Authorization:Bearer $access_token"
--- httpie_token_req.sh
+++ httpie_token_req.sh
@@ -4,17 +4,20 @@ REALM=master
 USER=admin
 PASSWORD=admin
 CLIENT_ID=demoapp
-HOST=https://secure-sso-sso.e8ca.engint.openshiftapps.com
-CLIENT=http://springboot-rest-sso.e8ca.engint.openshiftapps.com
+SSO_HOST=${1:-https://secure-sso-sso.e8ca.engint.openshiftapps.com}
+APP=${2:-http://springboot-rest-sso.e8ca.engint.openshiftapps.com}
 
 echo ">>> HTTP Token query"
-echo "http --verify=no -f $HOST/auth/realms/$REALM/protocol/openid-connect/token username=$USER password=$PASSWORD grant_type=password client_id=$CLIENT_ID"
+echo "http --verify=no -f $SSO_HOST/auth/realms/$REALM/protocol/openid-connect/token username=$USER password=$PASSWORD grant_type=password client_id=$CLIENT_ID"
 
-auth_result=$(http --verify=no -f $HOST/auth/realms/$REALM/protocol/openid-connect/token username=$USER password=$PASSWORD grant_type=password client_id=$CLIENT_ID)
+auth_result=$(http --verify=no -f $SSO_HOST/auth/realms/$REALM/protocol/openid-connect/token username=$USER password=$PASSWORD grant_type=password client_id=$CLIENT_ID)
 access_token=$(echo -e "$auth_result" | awk -F"," '{print $1}' | awk -F":" '{print $2}' | sed s/\"//g | tr -d ' ')
 
 echo ">>> TOKEN Received"
 echo $access_token
 
-echo ">>> Greeting Service"
-http --verify=no GET $CLIENT/greeting "Authorization: Bearer $access_token"
+echo ">>> Greeting"
+http --verify=no GET $APP/greeting "Authorization: Bearer $access_token"
+
+echo ">>> Greeting Customized Message"
+http --verify=no GET $APP/greeting name==spring "Authorization: Bearer $access_token"
