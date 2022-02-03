2217c7a43a1e1032dd71785d388c382f7fbc941a
Jose <josecarvajalhilario@gmail.com>
2022-02-03 08:02:57 +0100

    Add HELM chart
    Fix https://github.com/snowdrop/secured-example/issues/89

---
+++ run_tests_with_helm.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+SOURCE_REPOSITORY_URL=${1:-https://github.com/snowdrop/secured-example}
+SOURCE_REPOSITORY_REF=${2:-sb-2.5.x}
+
+source scripts/waitFor.sh
+
+# Deploy SSO
+oc apply -f .openshiftio/sso.yaml
+if [[ $(waitFor "sso" "application") -eq 1 ]] ; then
+  echo "SSO failed to deploy. Aborting"
+  exit 1
+fi
+SSO_URL=$(oc get route secure-sso -o jsonpath='https://{.spec.host}/auth')
+
+helm install secured ./helm --set spring-boot-example-app.s2i.source.repo=$SOURCE_REPOSITORY_URL --set spring-boot-example-app.s2i.source.ref=$SOURCE_REPOSITORY_REF --set spring-boot-example-app.s2i.env[0].name="MAVEN_ARGS_APPEND" --set spring-boot-example-app.s2i.env[0].value="-DSSO_AUTH_SERVER_URL=${SSO_URL}"
+if [[ $(waitFor "rest-secured" "app") -eq 1 ]] ; then
+  echo "Application failed to deploy. Aborting"
+  exit 1
+fi
+
+# Run OpenShift Tests
+./mvnw -s .github/mvn-settings.xml clean verify -Popenshift,openshift-it -Dunmanaged-test=true -DSSO_AUTH_SERVER_URL=$SSO_URL
