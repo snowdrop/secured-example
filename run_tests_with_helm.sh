c5598596f00517130b4c535e6a00741028600706
Jose <josecarvajalhilario@gmail.com>
2022-08-09 11:35:42 +0200

    Bump Java to 11

--- run_tests_with_helm.sh
+++ run_tests_with_helm.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 SOURCE_REPOSITORY_URL=${1:-https://github.com/snowdrop/secured-example}
 SOURCE_REPOSITORY_REF=${2:-sb-2.5.x}
+S2I_BUILDER_IMAGE_REPO=registry.access.redhat.com/ubi8/openjdk-11
+S2I_BUILDER_IMAGE_TAG=1.14
 
 source scripts/waitFor.sh
 
@@ -12,7 +14,7 @@ if [[ $(waitFor "sso" "application") -eq 1 ]] ; then
 fi
 SSO_URL=$(oc get route secure-sso -o jsonpath='https://{.spec.host}/auth')
 
-helm install secured ./helm --set spring-boot-example-app.s2i.source.repo=$SOURCE_REPOSITORY_URL --set spring-boot-example-app.s2i.source.ref=$SOURCE_REPOSITORY_REF --set spring-boot-example-app.s2i.env[0].name="MAVEN_ARGS_APPEND" --set spring-boot-example-app.s2i.env[0].value="-DSSO_AUTH_SERVER_URL=${SSO_URL}"
+helm install secured ./helm --set spring-boot-example-app.s2i.source.repo=$SOURCE_REPOSITORY_URL --set spring-boot-example-app.s2i.source.ref=$SOURCE_REPOSITORY_REF --set spring-boot-example-app.s2i.env[0].name="MAVEN_ARGS_APPEND" --set spring-boot-example-app.s2i.env[0].value="-DSSO_AUTH_SERVER_URL=${SSO_URL}" --set spring-boot-example-app.s2i.builderImage.repo=$S2I_BUILDER_IMAGE_REPO --set spring-boot-example-app.s2i.builderImage.tag=$S2I_BUILDER_IMAGE_TAG
 if [[ $(waitFor "rest-secured" "app") -eq 1 ]] ; then
   echo "Application failed to deploy. Aborting"
   exit 1
